895b986dd4349d33ae6e121919f3866acfc92af4
GabaMeurer <josegabrielmeurer@gmail.com>
2023-07-30 03:43:11 -0300

    adicao de nova home page e compomentes

---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Card, Button } from 'react-bootstrap';
+
+interface MyCardProps {
+  title: string;
+  content: string | JSX.Element;
+}
+
+const ProjectCard: React.FC<MyCardProps> = ({ title, content }) => (
+  <Card style={{ width: '18rem' }}>
+    <Card.Body>
+      <Card.Title>{title}</Card.Title>
+      <Card.Text>{content}</Card.Text>
+    </Card.Body>
+  </Card>
+);
+
+export default ProjectCard;
+
